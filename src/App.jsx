a3118846b7ab52d9ab44a35700a3019cac75e1ed
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-09 20:08:32 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center text-cyan-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
