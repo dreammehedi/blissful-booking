44d1e9a09817f0839862d77ac48e7b9b2b2830a9
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-10 12:31:53 +0600

    Newsletter

---
+++ src/components/newsletter/Newsletter.jsx
@@ -0,0 +1,34 @@
+function Newsletter() {
+  return (
+    <section className="py-8 md:py-12">
+      <div className="container flex flex-col space-y-3 justify-center items-center text-center md:max-w-2xl md:mx-auto">
+        <div>
+          <h1 className="text-primary font-bold text-3xl font-dmsans">
+            Subscribe to Our Newsletter{" "}
+          </h1>
+          <p className="text-slate-400">
+            Be the first to know about irresistible hotel booking, exclusive
+            packages, and insider tips straight to your inbox! Subscribe to our
+            newsletter and elevate your travel experience with access to
+            unbeatable offers tailored just for you.
+          </p>
+        </div>
+
+        <form className="relative py-6 md:py-8 w-full md:max-w-md md:mx-auto">
+          <input
+            className="w-full ring-1 ring-dark focus:ring-primary my-transition rounded-full px-6 py-3 font-medium font-poppins outline-none"
+            type="email"
+            placeholder="Your Email Address..."
+          />
+          <input
+            className="px-4 py-[10px] rounded-full bg-primary text-white font-medium cursor-pointer my-transition hover:bg-primary/80 absolute top-1/2 -translate-y-1/2 right-1"
+            type="submit"
+            value={"Subscribe"}
+          />
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Newsletter;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/banner/Banner";
 import Maps from "../../components/maps/Maps";
+import Newsletter from "../../components/newsletter/Newsletter";
 
 function Home() {
   return (
     <>
       <Banner></Banner>
       <Maps></Maps>
+      <Newsletter></Newsletter>
     </>
   );
 }
