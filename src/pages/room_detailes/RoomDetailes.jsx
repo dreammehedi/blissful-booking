3703711179dd59548a46169cf2e580f4c9960671
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-12 21:50:03 +0600

    User Booked Data Local Storage Set In Array Formate

--- src/pages/room_detailes/RoomDetailes.jsx
+++ src/pages/room_detailes/RoomDetailes.jsx
@@ -63,11 +63,18 @@ function RoomDetailes() {
     room_rules,
     available,
   } = data;
-
+  // get local storage data
+  function getLocalStorageData(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : [];
+  }
   // handle room booking
   const handleRoomBooking = () => {
     document.getElementById("my_modal_1").showModal();
   };
+
+  // user booked data stored in array
+  let userBookedRoom = getLocalStorageData("myBooking");
 
   // handle confirm booking
   const handleConfirmBooking = (roomId) => {
@@ -82,7 +89,8 @@ function RoomDetailes() {
       );
       const data = await response.data;
       if (data.modifiedCount > 0) {
-        localStorage.setItem("myBooking", JSON.stringify(roomId));
+        userBookedRoom.push(roomId);
+        localStorage.setItem("myBooking", JSON.stringify(userBookedRoom));
         toast.success("Hotel Booked successfully.");
         document.getElementById("my_modal_1").close();
         refetch();
