9400e8b371bf3eb296e5466710cd588a762b9f74
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-13 12:17:24 +0600

    Special Offer Promotion Modal Ui Ok

---
+++ src/components/special_offers_promotions/SpecialOfferPromotion.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { CgCloseO } from "react-icons/cg";
+
+function SpecialOfferPromotion() {
+  // modal control
+  const [showOffer, setShowOffer] = useState(false);
+  useEffect(() => {
+    setShowOffer(true);
+  }, []);
+
+  //   handle closer offer
+  const handleCloseOffer = () => {
+    setShowOffer(false);
+  };
+  return (
+    <>
+      {showOffer && (
+        <section
+          id="specialOfferPromotion"
+          className=" fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[99999] bg-dark/90 flex justify-center items-center w-full h-screen"
+        >
+          <div className="max-w-2xl mx-auto flex justify-center items-center flex-col p-8 rounded-tr-[120px] space-y-3 text-center rounded-bl-[120px] hover:bg-primary/95 my-transition bg-primary/90 text-white group relative">
+            <h1 className="text-white font-bold font-dmsans text-3xl">
+              Romantic Retreat Special
+            </h1>
+            <p className="text-slate-100">
+              Reignite the romance with our special retreat package. Champagne,
+              roses, and a cozy evening await.
+            </p>
+            <div className="flex justify-center items-center gap-6">
+              <button className="px-5 py-3 bg-primary/80 my-transition hover:shadow hover:shadow-primary hover:bg-primary text-white font-bold mr-auto rounded-tr-3xl rounded-bl-3xl  ring-1 group-hover:ring-white hover:rounded-3xl ">
+                $249 per night
+              </button>
+              <button className="px-5 py-3 bg-primary/80 my-transition hover:shadow hover:shadow-primary hover:bg-primary text-orange-400 font-bold mr-auto rounded-tr-3xl rounded-bl-3xl ring-1 group-hover:ring-white hover:rounded-3xl ">
+                ★★★★★
+              </button>
+            </div>
+            <button onClick={handleCloseOffer} className="text-4xl">
+              <CgCloseO></CgCloseO>
+            </button>
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
+
+export default SpecialOfferPromotion;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../components/banner/Banner";
 import FeaturedRooms from "../../components/featured_rooms/FeaturedRooms";
 import Maps from "../../components/maps/Maps";
 import Newsletter from "../../components/newsletter/Newsletter";
+import SpecialOfferPromotion from "../../components/special_offers_promotions/SpecialOfferPromotion";
 import UserReviews from "../../components/user_reviews/UserReviews";
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <Helmet>
         <title>Blissful Booking | Home</title>
       </Helmet>
+      <SpecialOfferPromotion></SpecialOfferPromotion>
       <Banner></Banner>
       <FeaturedRooms></FeaturedRooms>
       <UserReviews></UserReviews>
