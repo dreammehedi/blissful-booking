f1355c27171173bbabd6b3253994fa48015d6229
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-14 11:30:19 +0600

    Rooms Page Error Handle

--- src/pages/rooms/Rooms.jsx
+++ src/pages/rooms/Rooms.jsx
@@ -38,83 +38,77 @@ function Rooms() {
     setPriceRange(e.target.value);
     setCurrentPriceRange(e.target.value);
   };
+
   return (
     <>
       <Helmet>
         <title>Blissful Booking | Rooms</title>
       </Helmet>
       {/* room page banner */}
-      <RoomBanner firstRoomBanner={firstRoomBanner}></RoomBanner>
+      {firstRoomBanner && (
+        <RoomBanner firstRoomBanner={firstRoomBanner}></RoomBanner>
+      )}
 
       <section className="py-8 md:py-12">
-        {/* section title */}
-        <SectionTitle
-          title="Explore Our Available Rooms"
-          description=" Discover the perfect accommodation for your stay from our selection of available rooms. Whether you're seeking a cozy retreat, a spacious suite, or something in between, we have options to suit every traveler's needs. Browse through our collection below and find the ideal room for your next getaway.."
-        ></SectionTitle>
+        {availableRooms.length > 0 ? (
+          <>
+            <SectionTitle
+              title="Explore Our Available Rooms"
+              description=" Discover the perfect accommodation for your stay from our selection of available rooms. Whether you're seeking a cozy retreat, a spacious suite, or something in between, we have options to suit every traveler's needs. Browse through our collection below and find the ideal room for your next getaway.."
+            ></SectionTitle>
 
-        {/* room filter by price */}
-        <div className="container py-8 my:py-12 flex flex-col items-center lg:items-end text-right space-y-3 justify-center lg:justify-end">
-          <form className="max-w-sm mx-auto lg:!ml-auto lg:!mr-0">
-            <label
-              htmlFor="countries"
-              className="flex items-center gap-2
+            {/* room filter by price */}
+            <div className="container py-8 my:py-12 flex flex-col items-center lg:items-end text-right space-y-3 justify-center lg:justify-end">
+              <form className="max-w-sm mx-auto lg:!ml-auto lg:!mr-0">
+                <label
+                  htmlFor="countries"
+                  className="flex items-center gap-2
                mb-2 text-xl font-medium text-primary dark:text-white"
-            >
-              <FaFilterCircleDollar className="text-xl"></FaFilterCircleDollar>
-              Filter Price Range
-            </label>
-            <input
-              type="range"
-              name="priceRange"
-              id="priceRange"
-              min={100}
-              max={1000}
-              value={priceRange}
-              onChange={handlePriceRange}
-              className="w-full"
-            />
-            <div className="flex justify-between text-gray-500">
-              <span
-                className={`${
-                  currentPriceRange !== 0 ? "text-primary" : undefined
-                } text-dark`}
-              >
-                ${currentPriceRange}
-              </span>
-              <span className="text-dark">$1000</span>
+                >
+                  <FaFilterCircleDollar className="text-xl"></FaFilterCircleDollar>
+                  Filter Price Range
+                </label>
+                <input
+                  type="range"
+                  name="priceRange"
+                  id="priceRange"
+                  min={100}
+                  max={1000}
+                  value={priceRange}
+                  onChange={handlePriceRange}
+                  className="w-full"
+                />
+                <div className="flex justify-between text-gray-500">
+                  <span
+                    className={`${
+                      currentPriceRange !== 0 ? "text-primary" : undefined
+                    } text-dark`}
+                  >
+                    ${currentPriceRange}
+                  </span>
+                  <span className="text-dark">$1000</span>
+                </div>
+              </form>
             </div>
-            {/* <select
-              id="countries"
-              className="bg-transparent ring-1 ring-dark text-dark text-sm rounded-lg outline-none focus:ring-primary block w-full p-2.5 "
-            >
-              <option disabled></option>
-              <option defaultValue>Choose a Range</option>
-              <option disabled></option>
-              <option value="100">$ 100</option>
-              <option value="200">$ 200</option>
-              <option value="300">$ 300</option>
-              <option value="400">$ 400</option>
-              <option value="500">$ 500</option>
-              <option value="600">$ 600</option>
-              <option value="700">$ 700</option>
-              <option value="800">$ 800</option>
-              <option disabled></option>
-            </select> */}
-          </form>
-        </div>
 
-        {/* available rooms */}
-        <div className="py-8 md:py-12 container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-          {availableRooms?.map((availableRoom) => {
-            return (
-              <RoomCart
-                key={availableRoom._id}
-                availableRoom={availableRoom}
-              ></RoomCart>
-            );
-          })}
-        </div>
+            {/* available rooms */}
+            <div className="py-8 md:py-12 container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+              {availableRooms?.map((availableRoom) => {
+                return (
+                  <RoomCart
+                    key={availableRoom._id}
+                    availableRoom={availableRoom}
+                  ></RoomCart>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <SectionTitle
+            title="All Rooms Are Already Booked"
+            description=" We're sorry, but all our rooms are currently booked. We apologize for any inconvenience this may cause. Please feel free to reach out if you have any questions or need assistance with alternative accommodations."
+          ></SectionTitle>
+        )}
       </section>
     </>
   );
